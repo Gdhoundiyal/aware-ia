89a501a68f3363a5feb35f175a1319e320273b03
faisal <faisal.excel2011@gmail.com>
2025-02-28 15:29:54 +0530

    signup api integrated

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL : "http://116.202.210.102:5001",
+    baseURL : "http://116.202.210.102:3081",
     headers : {
 
     }
